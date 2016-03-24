0f6b317422019228785848ea766e32fab55de802
Darren Foong <darrenfoong@gmail.com>
2016-03-24 17:33:18 +0000

    Create an.txt

--- create_an_lexicon.py
+++ create_an_lexicon.py
@@ -3,13 +3,15 @@
 VECTORS_FILE_PATH = "data/vectors-sk-an.lemmas.50-min-count.15-iters"
 ADJECTIVES_FILE_PATH = "data/adjectives.txt"
 NOUNS_FILE_PATH = "data/nouns.txt"
+AN_FILE_PATH = "data/an.txt"
 
 adjectives = set()
 nouns = set()
 
 with open(VECTORS_FILE_PATH, "r") as vectors_file, \
      open(ADJECTIVES_FILE_PATH, "w") as adjectives_file, \
-     open(NOUNS_FILE_PATH, "w") as nouns_file:
+     open(NOUNS_FILE_PATH, "w") as nouns_file, \
+     open(AN_FILE_PATH, "w") as an_file:
 
      for line in iter(vectors_file):
          entry = line.split(" ")[0]
@@ -17,6 +19,7 @@ with open(VECTORS_FILE_PATH, "r") as vectors_file, \
              entry_words = entry.split("_")
              adjectives.add(entry_words[1])
              nouns.add(entry_words[2])
+             an_file.write(entry_words[1] + " " + entry_words[2] + "\n")
 
      print str(len(adjectives)) + " unique adjectives"
      print str(len(nouns)) + " unique nouns"
